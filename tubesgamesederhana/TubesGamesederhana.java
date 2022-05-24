e6672cd632b5a9f364fa81e82a4d06530cd4211d
Riko dan Yodya <99266700+RikoDanYodya@users.noreply.github.com>
2022-05-24 21:15:20 +0800

    Add files via upload

---
+++ tubesgamesederhana/TubesGamesederhana.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tubesgamesederhana;
+
+/**
+ *
+ * @author user
+ */
+public class TubesGamesederhana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frameLoading rg = new frameLoading();
+        rg.setVisible(true);
+        rg.pack();
+        rg.setLocationRelativeTo(null);
+        rg.setDefaultCloseOperation(frameLogin.EXIT_ON_CLOSE);
+    }
+    
+}
